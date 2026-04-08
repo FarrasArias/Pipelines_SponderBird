3e8bfa288d06f437080fed148ede724c7e23cab3
VFS\rgonzalez <rgonzalez@vfs.com>
2026-04-08 12:39:04 -0700

    Added firebase init call

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -31,6 +31,11 @@ public class FirebaseManager : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        InitFirebaseBridge();
+    }
+
     public void OnAuthReceived(string json)
     {
         Debug.Log($"Auth Received: {json}");
